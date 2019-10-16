a9aee512ef98e6e3bb5db8ad4ecda0f937b1a153
Nicolas Sentout <nicolas.sentout@hotmail.com>
2019-10-16 21:44:44 +0200

    improve window layout

--- src/application/MainWindow.java
+++ src/application/MainWindow.java
@@ -5,13 +5,20 @@ import java.util.Timer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
  
 public class MainWindow extends Application
@@ -19,8 +26,8 @@ public class MainWindow extends Application
 	private static final int START_DELAY = 3000;	// delay before the first auto press
 	private static final int TIME_BETWEEN_KEYPRESS = 1000;	// 10 minutes (600000)
 	
-	private static final int WINDOW_WIDTH = 230;
-	private static final int WINDOW_HEIGHT = 230;
+	private static final int WINDOW_WIDTH = 400;
+	private static final int WINDOW_HEIGHT = 400;
 	
 	private UserInput userInputChoice;
 	
@@ -32,54 +39,53 @@ public class MainWindow extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        FlowPane root = new FlowPane();
-        
         userInputChoice = new UserInput();
-  
-        Button mainButton = new Button("ACTIVER AUTO PRESS");
-        //mainButton.setMinSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         
-        Button saveAutoPressKeyButton = new Button("PRESS TO SAVE THE KEY YOU WANT TO AUTO PRESS");
+        Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
         
-        // Autopress key
-        Label keyLabel = new Label();
+        Button autoPressButton = (Button) root.lookup("#autoPressButton");
+        Button defineKeyButton = (Button) root.lookup("#defineKeyButton");
+        Label keyLabel = (Label) root.lookup("#autoPressedKey");
+        keyLabel.setBackground(new Background(new BackgroundFill(Color.rgb(230, 230, 230), CornerRadii.EMPTY, Insets.EMPTY)));
         
         // Activate / Desactivate auto pressing
-        mainButton.setOnAction(new EventHandler<ActionEvent>() {
+        autoPressButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event)
             {
             	if (isAutoTyping) {
-            		mainButton.setText("ACTIVER AUTO PRESS");
+            		autoPressButton.setText("ACTIVER AUTOPRESS");
             		timer.cancel();
             	}
             	else {
-            		mainButton.setText("DESACTIVER AUTO PRESS");
+            		autoPressButton.setText("DÉSACTIVER AUTOPRESS");
             		timer = new Timer();
             		timer.schedule(userInputChoice, START_DELAY, TIME_BETWEEN_KEYPRESS);
             	}
-            	
         		isAutoTyping = !isAutoTyping;
             }
         });
         
         // Ask the user to input the new auto pressing key
-        saveAutoPressKeyButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        defineKeyButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent me)
             {
             	if (me.getButton() == MouseButton.PRIMARY) {
-                   	saveAutoPressKeyButton.setText("?");
+                   	defineKeyButton.setText("?");
                 	isChangingKey = true;
             	}
             }
         });
         
         // Save the new auto pressing key
-        saveAutoPressKeyButton.setOnKeyPressed(new EventHandler<KeyEvent>() {
+        defineKeyButton.setOnKeyPressed(new EventHandler<KeyEvent>() {
             public void handle(KeyEvent ke)
             {
             	if (isChangingKey) {
             		System.out.println("Key that will be autopressed: " + ke.getCode());
+            		
+            		// Center the click button label
+            		((HBox) root.lookup("#defineKeyBox")).setSpacing(150.0);
             		
             		// Update the key that will be auto pressing
             		userInputChoice.setUserInput(ke);
@@ -88,7 +94,7 @@ public class MainWindow extends Application
             		keyLabel.setText(ke.getCode().getName());
             		
             		// Reset the button allowing to change the key that will be auto pressing
-            		saveAutoPressKeyButton.setText("Type the key you want to autopress");
+            		defineKeyButton.setText("Définir touche");
             		isChangingKey = false;
             	}
             }
@@ -101,6 +107,9 @@ public class MainWindow extends Application
             	if (isChangingKey) {
             		System.out.println("Mouse : " + me.getButton().name());
             		
+            		// Center the click button label
+            		((HBox) root.lookup("#defineKeyBox")).setSpacing(40.0);
+            		
             		// Update the key that will be auto pressing
             		userInputChoice.setUserInput(me);
             		
@@ -108,17 +117,16 @@ public class MainWindow extends Application
             		keyLabel.setText(me.getButton().name());
             		
             		// Reset the button allowing to change the key that will be auto pressing
-            		saveAutoPressKeyButton.setText("Type the key you want to autopress");
+            		defineKeyButton.setText("Définir touche");
             		isChangingKey = false;
             	}
             }
         });
-
-        root.getChildren().addAll(mainButton, saveAutoPressKeyButton, keyLabel);
- 
-        primaryStage.setTitle("AutoKeyPress");
- 
+        
         Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        
+        primaryStage.setTitle("AutoKeyPress");
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
