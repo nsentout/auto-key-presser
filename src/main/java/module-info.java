9689a6b638f158b7beb002f3191c4c04e9e3fc35
Nicolas Sentout <nicodufcbas@hotmail.fr>
2020-04-09 13:36:15 +0200

    readd runnable jar file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module autokeypresser {
+	exports fiftiz.autokeypresser;
+
+	requires java.desktop;
+	requires java.logging;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires transitive javafx.graphics;
+	requires transitive jnativehook;
+}
